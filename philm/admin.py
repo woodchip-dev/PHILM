2c9f16e012f147d457137bf3883856717461484d
amitchell <amitchell@alaska.edu>
2019-03-23 00:52:43 -0800

    Add misc config files

---
+++ philm/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from philm.models import Film, Years, Genres, Person, Reviews
+
+
+admin.site.register(Film)
+admin.site.register(Years)
+admin.site.register(Genres)
+admin.site.register(Person)
+admin.site.register(Reviews)
