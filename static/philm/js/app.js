328dbc3774151b80fd20156623993fc6eb94c49c
amitchell <amitchell@alaska.edu>
2019-03-23 00:48:19 -0800

    Add JS project file

---
+++ static/philm/js/app.js
@@ -0,0 +1,37 @@
+$(document).foundation()
+
+
+function selected(id)
+{
+    var element = document.getElementById(id);
+    var ehidden = element.nextSibling.nextSibling;
+    console.log(ehidden);
+
+    if($(element).hasClass('marked-in'))
+    {
+        ehidden.setAttribute("value", "2")
+        $(element).removeClass('marked-in');
+        element.removeChild(element.childNodes[0])
+
+        $(element).addClass('marked-ex');
+        var node = document.createElement('i');
+        $(node).addClass('fas');
+        $(node).addClass('fa-times');
+        element.appendChild(node);
+    }
+    else if($(element).hasClass('marked-ex'))
+    {
+        ehidden.setAttribute("value", "0")
+        $(element).removeClass('marked-ex');
+        element.removeChild(element.childNodes[0])
+    }
+    else
+    {
+        ehidden.setAttribute("value", "1")
+        $(element).addClass('marked-in');
+        var node = document.createElement('i');
+        $(node).addClass('fas');
+        $(node).addClass('fa-check');
+        element.appendChild(node);
+    }
+}
